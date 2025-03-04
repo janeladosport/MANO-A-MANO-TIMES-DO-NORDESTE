6eafc10109c7b11352ebba6f1fa21785476dc677
janeladosport <ccgamer053@gmail.com>
2025-03-04 16:43:27 -0300

    v14

--- escale.js
+++ escale.js
@@ -34,18 +34,46 @@ var pdFortaleza = ["Ponta direita", "pd-esc", "imagens/fortaleza/pikachu.png", "
 var peFortaleza = ["Ponta esquerda", "pe-esc", "imagens/fortaleza/moises.png", "imagens/fortaleza/breno-lopes.png"]
 var caFortaleza = ["Centroavante", "ca-esc", "imagens/fortaleza/lucero.png", "imagens/fortaleza/renato-kayzer.png", "imagens/fortaleza/dylan-borrero.png"]
 
+var golVitoria = ["Goleiro", "gol-esc", "imagens/vitoria/lucas-arcanjo.png", "imagens/vitoria/gabriel.png", "imagens/vitoria/fintelman.png"]
+var ldVitoria = ["Lateral Direito", "ld-esc", "imagens/vitoria/claudinho.png", "imagens/vitoria/raul-caceres.png"]
+var leVitoria = ["Lateral Esquerdo", "le-esc", "imagens/vitoria/jamerson.png", "imagens/vitoria/hugo.png", "imagens/vitoria/ze-marcos.png"]
+var zdVitoria = ["Zagueiro Direito", "zd-esc", "imagens/vitoria/neris.png", "imagens/vitoria/lucas-halter.png", "imagens/vitoria/camutanga.png", "imagens/vitoria/edu.png"]
+var zeVitoria = ["Zaguerio Esquerdo", "ze-esc", "imagens/vitoria/lucas-halter.png", "imagens/vitoria/ze-marcos.png", "imagens/vitoria/neris.png"]
+var volVitoria = ["1º Volante", "vol-esc", "imagens/vitoria/baralhas.png", "imagens/vitoria/dionisio.png", "imagens/vitoria/ricardo-ryller.png", "imagens/vitoria/val-soares.png", "imagens/vitoria/william-oliveira.png", "imagens/vitoria/thiago-conti.png"]
+var mcVitoria = ["2º Volante", "mc-esc", "imagens/vitoria/pepê.png", "imagens/vitoria/ronald.png", "imagens/vitoria/dionisio.png", "imagens/vitoria/val-soares.png", "imagens/vitoria/william-oliveira.png", "imagens/vitoria/thiago-conti.png"]
+var meiVitoria = ["Meia", "mei-esc", "imagens/vitoria/matheusinho.png", "imagens/vitoria/wellington-rato.png", "imagens/vitoria/pablo.png", "imagens/vitoria/pepê.png"]
+var pdVitoria = ["Ponta direita", "pd-esc", "imagens/vitoria/gustavo-mosquito.png", "imagens/vitoria/lucas-braga.png", "imagens/vitoria/wellington-rato.png", "imagens/vitoria/matheusinho.png", "imagens/vitoria/janderson.png"]
+var peVitoria = ["Ponta esquerda", "pe-esc", "imagens/vitoria/lucas-braga.png", "imagens/vitoria/wellington-rato.png", "imagens/vitoria/carlos-eduardo.png", "imagens/vitoria/bruno-xavier.png"]
+var caVitoria = ["Centroavante", "ca-esc", "imagens/vitoria/janderson.png", "imagens/vitoria/fabri.png", "imagens/vitoria/carlinhos.png"]
+
 var dados = {
     Sport: [golSport, ldSport, zdSport, zeSport, leSport, volSport, mcSport, meiSport, pdSport, peSport, caSport],
     Bahia: [golBahia, ldBahia, zdBahia, zeBahia, leBahia, volBahia, mcBahia, meiBahia, pdBahia, peBahia, caBahia],
-    Fortaleza: [golFortaleza, ldFortaleza, zdFortaleza, zeFortaleza, leFortaleza, volFortaleza, mcFortaleza, meiFortaleza, pdFortaleza, peFortaleza, caFortaleza]
+    Fortaleza: [golFortaleza, ldFortaleza, zdFortaleza, zeFortaleza, leFortaleza, volFortaleza, mcFortaleza, meiFortaleza, pdFortaleza, peFortaleza, caFortaleza],
+    Vitoria:[golVitoria, ldVitoria, zdVitoria, zeVitoria, leVitoria, volVitoria, mcVitoria, meiVitoria, pdVitoria, peVitoria, caVitoria]
 };
 // , ldSport, zdSport, zeSport, leSport, volSport, mcSport, meiSport, pdSport, peSport, caSport
 
-
 function selecionarEscale(elemento){
-    limparEscolhaEscale()
-    limparCampoEscale()
     var time = elemento.value
+    botarEscale(time, true, false, "", false)
+}
+
+function botarEscale(time, clean, perso, numb, check){
+    console.log("check: ", check)
+    if (check == true){
+        console.log("Checando")
+        if (document.getElementById("escolha")){
+            document.getElementById("escolha").remove()
+            return false
+        } else{
+            createEscolha()
+        }
+    }
+    if (clean == true){
+        limparEscolhaEscale()
+    }
+    limparCampoEscale()
     let timeValor = dados[time]
     for (i=0; i < timeValor.length; i++){
         let div = document.createElement("div")
@@ -55,19 +83,27 @@ function selecionarEscale(elemento){
         let id = timeI[1]
         span.innerHTML = nome
         let escolha = document.getElementById("escolha")
-        escolha.appendChild(div)
-        span.setAttribute("class", "span-escolha")
-        div.appendChild(span)
-        div.setAttribute("class", "nome-escolha")
-        for (h=2; h < timeI.length; h++){
-            let source = timeI[h]
-            let imagem = document.createElement("img")
-            imagem.setAttribute("src", source)
-            div.appendChild(imagem)
-            imagem.setAttribute("class", "jogador-selectable")
-            imagem.setAttribute("onclick", "clicarEscale(this)")
-            imagem.setAttribute("id", id)
+        if (escolha){
+            escolha.appendChild(div)
+            span.setAttribute("class", "span-escolha")
+            div.appendChild(span)
+            div.setAttribute("class", "nome-escolha")
+            for (h=2; h < timeI.length; h++){
+                let source = timeI[h]
+                let imagem = document.createElement("img")
+                imagem.setAttribute("src", source)
+                div.appendChild(imagem)
+                imagem.setAttribute("class", "jogador-selectable")
+                imagem.setAttribute("data-info", time)
+                imagem.setAttribute("onclick", "clicarEscale(this)")
+                if (perso == true){
+                    imagem.removeAttribute("onclick")
+                    imagem.setAttribute("onclick", `alterarTime(this, '${numb}')`)
+                }
+                imagem.setAttribute("id", id)
+            }
         }
+        
     }
 }
 
@@ -86,6 +122,10 @@ function limparCampoEscale(){
 }
 
 function clicarEscale(elemento){
+    let ver = verificarDuplicata("jogador", elemento.src)
+    if (ver == false){
+        return
+    }
     if (elemento.id == "gol-esc"){
         document.getElementById("gol").src = elemento.src
     } if (elemento.id == "ld-esc"){
@@ -109,34 +149,142 @@ function clicarEscale(elemento){
     } if (elemento.id == "ca-esc"){
         document.getElementById("ca").src = elemento.src
     }
-    console.log("MUDOU")
 
 
-    verificarDuplicata()
+    
 }
 
-function verificarDuplicata(){
-    let imagens = document.getElementsByClassName("jogador"); // Seleciona apenas as imagens com essa classe
-    console.log("Imagens: ", imagens)
+function alterarTime(elemento, numb){
+    let numeros = {
+        "gol-esc": 0,
+        "ld-esc": 1,
+        "zd-esc": 2,
+        "ze-esc": 3,
+        "le-esc": 4,
+        "vol-esc": 5,
+        "mc-esc": 6,
+        "mei-esc": 7,
+        "pd-esc": 8,
+        "pe-esc": 9,
+        "ca-esc": 10
+    }
+    let verificate = verificarDuplicataAntes("item", elemento.src)
+    console.log("Verificate: ", verificate)
+    if (verificate == true) {
+        timesFut[elemento.dataset.info][numeros[elemento.id]] = elemento.src
+        if (numb = "um"){
+            botarUm(timeUno, false)
+        } if (numb = "dois"){
+            botarDois(timeDuno, false)
+        }}
+    // verificarDuplicata("item", true)
+}
+
+// function verificarDuplicata(classe, query){
+//     console.log("Classe: ", classe)
+//     let imagens = document.getElementsByClassName(classe);
+//     let srcSet = new Set();
+//     let permitidoCount = 0;
+//     let permitido = "branco.png"
+//     let nomeArquivo
+//     for (let img of imagens) {
+//         console.log("IMG: ", img)
+//         console.log("Imagem source: ", img.src)
+//         if (query == true){
+//             let imgTrue = img.querySelector("img")
+//             console.log(imgTrue.src)
+//             nomeArquivo = imgTrue.src.substring(imgTrue.src.lastIndexOf("/") + 1) 
+//         } else{
+//             nomeArquivo = img.src.substring(img.src.lastIndexOf("/") + 1) 
+//         }
+        
+//         console.log("SOURCE: ", nomeArquivo)
+//         if (nomeArquivo == permitido) {
+//             permitidoCount++;
+//             continue;
+//         }
+//         if (srcSet.has(nomeArquivo)) {
+//             limparCampoEscale()
+//             alert("Não selecione jogadores iguais")
+//             return false;
+//         }
+        
+//         srcSet.add(nomeArquivo);
+//     }
+
+//     return true;
+// }
+
+// function verificarDuplicata(classe){
+//     let imagens = document.getElementsByClassName(classe);
+//     let srcSet = new Set();
+//     let permitidoCount = 0;
+//     let permitido = "branco.png"
+//     let nomeArquivo
+//     for (let img of imagens) {
+//         nomeArquivo = img.src.substring(img.src.lastIndexOf("/") + 1) 
+//         if (nomeArquivo == permitido) {
+//             permitidoCount++;
+//             continue;
+//         }
+//         if (srcSet.has(nomeArquivo)) {
+//             // limparCampoEscale()
+//             alert("Não selecione jogadores iguais")
+//             return false;
+//         }
+        
+//         srcSet.add(nomeArquivo);
+//     }
+
+//     return true;
+// }
+
+function verificarDuplicata(classe, source){
+    let imagens = document.getElementsByClassName(classe);
     let srcSet = new Set();
     let permitidoCount = 0;
     let permitido = "branco.png"
-    for (let img of imagens) {
-        console.log("IMG: ", img)
-        console.log("Imagem source: ", img.src)
-        let nomeArquivo = img.src.substring(img.src.lastIndexOf("/") + 1) 
-        console.log("SOURCE: ", nomeArquivo)
-        if (nomeArquivo == permitido) {
+   
+    for (i=0; i < imagens.length; i++) {
+        nomeArquivoAtual = source.substring(source.lastIndexOf("/") + 1) 
+        nomeArquivo = imagens[i].src.substring(imagens[i].src.lastIndexOf("/") + 1)
+        srcSet.add(nomeArquivo);
+        if (nomeArquivoAtual == permitido) {
             permitidoCount++;
-            continue;
+            continue; 
         }
-        if (srcSet.has(img.src)) {
-            limparCampoEscale()
+        if (srcSet.has(nomeArquivoAtual)) {
             alert("Não selecione jogadores iguais")
             return false;
         }
         
-        srcSet.add(img.src);
+    }
+
+    return true;
+}
+
+function verificarDuplicataAntes(classe, sourceAntes){
+    let imagens = document.getElementsByClassName(classe);
+    let srcSet = new Set();
+    let permitidoCount = 0;
+    let permitido = "branco.png"
+   
+    for (i=0; i < imagens.length; i++) {
+
+        let sourceImagens = imagens[i].querySelector("img").src
+        nomeArquivoAtual = sourceAntes.substring(sourceAntes.lastIndexOf("/") + 1) 
+        console.log(nomeArquivoAtual)
+        nomeArquivo = sourceImagens.substring(sourceImagens.lastIndexOf("/") + 1)
+        srcSet.add(nomeArquivo);
+        if (nomeArquivoAtual == permitido) {
+            permitidoCount++;
+            continue; 
+        }
+        if (srcSet.has(nomeArquivoAtual)) {
+            alert("Não selecione jogadores iguais")
+            return false;
+        }
+        
     }
 
     return true;
