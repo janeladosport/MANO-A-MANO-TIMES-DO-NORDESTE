1022952d34f02d582132c1aebc30e976f450c81e
janeladosport <ccgamer053@gmail.com>
2025-05-11 08:40:46 -0300

    new mecanism

--- palpitometro.js
+++ palpitometro.js
@@ -1,40 +1,23 @@
-escudos = {
-    'fluminense': 'imagens/escudos/fluminense.png',
-    'sport': 'imagens/escudos/sport.png',
-    'corinthians': 'imagens/escudos/corinthians.png',
-    'flamengo': 'imagens/escudos/flamengo.png',
-    'saopaulo': 'imagens/escudos/sao-paulo.png',
-    'botafogo': 'imagens/escudos/botafogo.png',
-    'bahia': 'imagens/escudos/bahia.png',
-    'vitoria': 'imagens/escudos/vitoria.png',
-    'cruzeiro': 'imagens/escudos/cruzeiro.png',
-    'atletico': 'imagens/escudos/atletico.png',
-    'santos': 'imagens/escudos/santos.png',
-    'bragantino': 'imagens/escudos/bragantino.png',
-    'ceara': 'imagens/escudos/ceara.png',
-    'gremio': 'imagens/escudos/gremio.png',
-    'inter': 'imagens/escudos/inter.png',
-    'juventude': 'imagens/escudos/juventude.png',
-    'mirassol': 'imagens/escudos/mirassol.png',
-    'palmeiras': 'imagens/escudos/palmeiras.png',
-    'vasco': 'imagens/escudos/vasco.png',
-    'fortaleza': 'imagens/escudos/fortaleza.png'
+function escudos(time){
+    if (time == "saopaulo"){time = 'sao-paulo'}
+    return `imagens/escudos/${time}.png`
 }
+
 jogos = [
-    ['Sport X Cruzeiro', escudos['sport'], escudos['cruzeiro'], ['sport', 'cruzeiro']],
-    ['Palmeiras X São Paulo', escudos['palmeiras'], escudos['saopaulo'], ['palmeiras', 'saopaulo']],
-    ['Atlético-MG X Fluminense', escudos['atletico'], escudos['fluminense'], ['atletico', 'fluminense']],
-    ['Botafogo X Internacional', escudos['botafogo'], escudos['inter'], ['botafogo', 'inter']],
-    ['Santos X Ceará', escudos['santos'], escudos['ceara'], ['santos', 'ceara'], true]
+    ['sport', 'cruzeiro'],
+    ['palmeiras', 'saopaulo'],
+    ['atletico', 'fluminense'],
+    ['botafogo', 'inter'],
+    ['fortaleza', 'juventude', [5,0]],
+    ['mirassol', 'corinthians', [2,1]],
+    ['gremio', 'bragantino', [1,1]],
+    ['vitoria', 'vasco', [2,1]],
+    ['flamengo', 'bahia',[1,0]],
+    ['santos', 'ceara', true]
 ]
 
 correspondencia = {
-    'ceara': "Ceará",
-    'vitoria': "Vitória",
-    'gremio': "Grêmio",
-    'atletico': "Atlético-MG",
-    "saopaulo": "São Paulo",
-    "bragantino": "RB Bragantino"
+    'ceara': "Ceará", 'vitoria': "Vitória", 'gremio': "Grêmio", 'atletico': "Atlético-MG", "saopaulo": "São Paulo", "bragantino": "RB Bragantino", 'inter': 'Internacional'
 }
 
 sport = srcConjunto("sport", ["caique-frança", "hereda", "joao-silva", "chico", "igor-carius", "christian-rivera", "ze-lucas", "sergio-oliveira", "chino-atencio", "barletta", "pablo"])
@@ -74,10 +57,7 @@ timesFut["Fluminense"] = fluminense
 
 function srcConjunto(time, jogadores){
     let conjunto = []
-    for (i=0; i < jogadores.length; i++){
-        conjunto.push(`imagens/${time}/${jogadores[i]}.png`)
-    }
-    console.log(conjunto)
+    for (i=0; i < jogadores.length; i++){conjunto.push(`imagens/${time}/${jogadores[i]}.png`)}
     return conjunto 
 }
 
@@ -87,20 +67,24 @@ function createGames(){
     for (i=0; i < jogos.length; i++){
         let newCabine = document.createElement("div")
         newCabine.classList.add("jogo-palpite")
-        newCabine.setAttribute("datainfo-um", jogos[i][3][0])
-        newCabine.setAttribute("datainfo-dois", jogos[i][3][1])
+        newCabine.setAttribute("datainfo-um", jogos[i][0])
+        newCabine.setAttribute("datainfo-dois", jogos[i][1])
         palpites.appendChild(newCabine)
 
         let span = document.createElement("span")
-        span.innerHTML = jogos[i][0]
+        let sTu; let sTd; let inputone; let inputtwo
+        
+        if (correspondencia[jogos[i][0]]){sTu = correspondencia[jogos[i][0]]} else {sTu = jogos[i][0][0].toUpperCase() + jogos[i][0].substring(1)}
+        if (correspondencia[jogos[i][1]]){sTd = correspondencia[jogos[i][1]]} else {sTd = jogos[i][1][0].toUpperCase() + jogos[i][1].substring(1)}
+        span.innerHTML = `${sTu} X ${sTd}`
         newCabine.appendChild(span)
         span.classList.add("palpite-span")
 
-        let escudoUm = jogos[i][1]
+        let escudoUm = escudos(jogos[i][0])
         let newImg = document.createElement("img")
         newImg.src = escudoUm
 
-        let escudoDois = jogos[i][2]
+        let escudoDois = escudos(jogos[i][1])
         let newImgD = document.createElement("img")
         newImgD.src = escudoDois
         newImg.classList.add("palpites-img")
@@ -109,10 +93,12 @@ function createGames(){
         let newInput = document.createElement("input")
         newInput.classList.add('palpites-input')
         newInput.setAttribute("type", "number")
+        inputone = newInput
 
         let newInputD = document.createElement("input")
         newInputD.classList.add('palpites-input')
         newInputD.setAttribute("type", "number")
+        inputtwo = newInputD
 
         let spanX = document.createElement("span")
         spanX.innerHTML = "X"
@@ -121,13 +107,19 @@ function createGames(){
         buttonMAM.classList.add("palpites-button-alone")
         buttonMAM.innerHTML = "MANO A MANO"
         buttonMAM.setAttribute("onclick", "mam(this)")
-        if (jogos[i][4]==true){buttonMAM.setAttribute("disabled",""); buttonMAM.style.backgroundColor =  'gray'; buttonMAM.style.cursor = "not-allowed"}
+        if (jogos[i][2]==true){buttonMAM.setAttribute("disabled",""); buttonMAM.style.backgroundColor =  'gray'; buttonMAM.style.cursor = "not-allowed"} else if (jogos[i][2]){
+            inputone = document.createElement('span')
+            inputone.innerHTML = jogos[i][2][0]
+            inputtwo = document.createElement('span')
+            inputtwo.innerHTML = jogos[i][2][1]
+            inputone.classList.add("result-span"); inputtwo.classList.add("result-span")
+        }
         
 
         newCabine.appendChild(newImg)
-        newCabine.appendChild(newInput)
+        newCabine.appendChild(inputone)
         newCabine.appendChild(spanX)
-        newCabine.appendChild(newInputD)
+        newCabine.appendChild(inputtwo)
         newCabine.appendChild(newImgD)
         newCabine.appendChild(buttonMAM)
 
@@ -141,7 +133,7 @@ function mam(element){
     let timeUm = palpites.getAttribute("datainfo-um")
     let timeDois = palpites.getAttribute("datainfo-dois")
     
-    selecionarUm({value:timeUm}, element.parentElement, escudos[timeUm], escudos[timeDois])
+    selecionarUm({value:timeUm}, element.parentElement, escudos(timeUm), escudos(timeDois))
     document.getElementById("titulo").scrollIntoView({ behavior: 'smooth' })
     selecionarDois({value:timeDois})
     
@@ -153,7 +145,7 @@ function selecionarUm(elemento, element, escUm, escDois){
     }
 time = elemento.value
 time = time[0].toUpperCase() + time.substring(1)
-imgFutUm = escudos[elemento.value]
+imgFutUm = escudos(elemento.value)
 nameFutUm = time
 createMAM(element, escUm, escDois)
 botarUm(time, false)
@@ -164,7 +156,7 @@ function selecionarDois(elemento){
     }
 time = elemento.value
 time = time[0].toUpperCase() + time.substring(1)
-imgFutUm = escudos[elemento.value]
+imgFutUm = escudos(elemento.value)
 nameFutUm = time
 botarDois(time, false)
 }
