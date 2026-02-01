00a526a811147d4c0046cb892f0fca0d0a807fd3
janeladosport <ccgamer053@gmail.com>
2026-01-31 22:02:49 -0300

    .

---
+++ janela.js
@@ -0,0 +1,119 @@
+timesNE = {
+    'Bahia': {
+        'link': 'imagens/escudos/bahia.png'
+    },
+    'Fortaleza': {
+        'link': 'imagens/escudos/fortaleza.png'
+    },
+    'Sport': {
+        'link': 'imagens/escudos/sport.png'
+    },
+    'Ceara': {
+        'link': 'imagens/escudos/ceara.png'
+    },
+    'Vitoria': {
+        'link': 'imagens/escudos/vitoria.png'
+    }
+}
+
+
+function draggerAdd(){
+    let newDiv = document.getElementsByClassName("player")
+    for (item in newDiv){
+    }
+}
+
+function putTime(team){
+    let bigBox = document.createElement("div")
+    bigBox.id = 'bigbox'
+    document.body.appendChild(bigBox)
+    let elenco = document.createElement("div")
+    elenco.id = "elenco"
+    bigBox.appendChild(elenco)
+    let elArray = dados[team]
+    for (pos in elArray){
+        let actual = elArray[pos]
+        let boxPosition = document.createElement("div")
+        boxPosition.classList.add("posicao")
+        elenco.appendChild(boxPosition)
+        let span = document.createElement("span")
+        span.innerHTML = actual[0]
+        let idcomon = actual[1]
+        boxPosition.appendChild(span)
+        for (num in actual){
+            if (num != 1 && num != 0){
+                let newDiv = document.createElement("div")
+                newDiv.style.backgroundImage = `url(${actual[num]})`
+                boxPosition.appendChild(newDiv)
+                newDiv.classList.add("player")
+                newDiv.setAttribute("draggable", 'true')
+            }
+        }
+    }
+}
+
+positions = ['Goleiro', 'Lateral direito', 'Lateral esquerdo', 'Zagueiro', '1º Volante', '2º Volante', 'Meia', 'Pontas', 'Centroavante']
+
+function putJanela(){
+    let bigBox = document.getElementById("bigbox")
+    let janelaIdeal = document.createElement("div")
+    janelaIdeal.id = "janelaideal"
+    bigBox.appendChild(janelaIdeal)
+    for (pos in positions){
+        let nome = positions[pos]
+        let newSpan = document.createElement("span")
+        newSpan.innerHTML = nome
+        let positionBox = document.createElement("div")
+        positionBox.classList.add("position-box")
+        let adicionar = document.createElement("div")
+        adicionar.classList.add("adicionar")
+        janelaIdeal.appendChild(newSpan)
+        janelaIdeal.appendChild(positionBox)
+        positionBox.appendChild(adicionar)
+        adicionar.addEventListener("click", (e) => {
+            let img = document.createElement("div")
+            img.classList.add("jogador-choice")
+            positionBox.prepend(img)
+            let close = document.createElement("div")
+            close.classList.add("close")
+            img.appendChild(close)
+            close.addEventListener("click", (e)=>{
+                e.target.parentElement.remove()    
+            })
+        })
+    }
+}
+
+function selectTeam(object){
+    elemento = object.target
+    time = elemento.getAttribute("data")
+    source = elemento.src
+    console.log(time)
+    document.getElementById("choiceBox").remove()
+    let image = document.createElement("img")
+    image.src = source
+    image.id = 'imageFixed'
+    document.body.appendChild(image)
+    putTime(time)
+    putJanela()
+}
+
+function putChoiceBox(){
+    let box = document.createElement("div")
+    box.id = "choiceBox"
+    document.body.appendChild(box)
+    let text = document.createElement('span')
+    text.innerHTML = "ESCOLHA O SEU TIME"
+    box.appendChild(text)
+    for (times in timesNE){
+        console.log(timesNE[times]['link'])
+        let team = document.createElement("img")
+        team.src = timesNE[times]['link']
+        team.setAttribute("data", times)
+        box.appendChild(team)
+        team.addEventListener("click", (e) => {selectTeam(e)})
+    }
+}
+
+putChoiceBox()
+
---
+++ teste.js
@@ -0,0 +1,51 @@
+let dragged = null;
+
+document.addEventListener("dragstart", e => {
+  const el = e.target.closest(".player");
+  if (!el) return;
+
+  dragged = el;
+  el.classList.add("dragging");
+});
+
+document.addEventListener("dragend", e => {
+  const el = e.target.closest(".player");
+  if (!el) return;
+
+  el.classList.remove("dragging");
+  dragged = null;
+});
+
+document.addEventListener("dragover", e => {
+  var container = e.target.closest(".posicao");
+  if (!container || !dragged) {
+    const newContainer = e.target.closest(".position-box")
+    if (!newContainer){
+        return;
+    } else{
+      container = newContainer
+    }
+  }
+  e.preventDefault();
+
+  const after = getAfterElement(container, e.clientX);
+  if (after == null) {
+    container.prepend(dragged);
+  } else {
+    container.insertBefore(dragged, after);
+  }
+});
+
+function getAfterElement(container, x) {
+  const els = [...container.querySelectorAll(".player:not(.dragging)")];
+
+  return els.reduce((closest, el) => {
+    const box = el.getBoundingClientRect();
+    const offset = x - box.left - box.width / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset, el };
+    }
+    return closest;
+  }, { offset: Number.NEGATIVE_INFINITY }).el;
+}
